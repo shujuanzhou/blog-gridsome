e2a5b9949cf80ef577271c5969777ce4f3afac2f
shujuanzhou <1182669484@qq.com>
2021-01-05 14:43:33 +0000

    Initial commit

---
+++ src/templates/Tag.vue
@@ -0,0 +1,59 @@
+<template>
+  <Layout>
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('/img/home-bg.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="site-heading">
+              <h1># {{ $page.tag.title }}</h1>
+              <!-- <span class="subheading">A Blog Theme by Start Bootstrap</span> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div
+            class="post-preview"
+            v-for="edge in $page.tag.posts"
+            :key="edge.id"
+          >
+            <g-link :to="'/post/' + edge.id">
+              <h2 class="post-title">
+                {{ edge.title }}
+              </h2>
+            </g-link>
+            <hr />
+          </div>
+          <!-- Pager -->
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query($id: ID){
+  tag:strapiTag(id: $id){
+    id
+    title
+    posts{
+      id
+      title
+    }
+  }
+}
+</page-query>
+<script>
+export default {
+  name: "TagPage",
+};
+</script>
+
+<style></style>
